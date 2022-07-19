d414b6b443a5f909da855a86fc68d5698d54378a
aemirdnr <aemirdnr@gmail.com>
2022-07-20 02:49:09 +0300

    Code Refactor

--- main.js
+++ main.js
@@ -1,158 +1,123 @@
-const buttonContainer = document.getElementById("buttons")
-const menuContainer = document.getElementById("menu-container")
-let categories = ['All']
+const buttonContainer = document.getElementById("buttons");
+const menuContainer = document.getElementById("menu-container");
+let categories = ["All"];
 
 let menu = [
-    {
-        id: 1,
-        title: "Tteokbokki",
-        category: "Korea",
-        price: 10.99,
-        img:
-          "https://twoplaidaprons.com/wp-content/uploads/2020/09/tteokbokki-top-down-view-of-tteokbokki-in-a-bowl-500x500.jpg",
-        desc: `Spicy rice cakes, serving with fish cake.`,
-    },
-    {
-        id: 2,
-        title: "Chicken Ramen",
-        category: "Japan",
-        price: 7.99,
-        img:
-          "https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",
-        desc: `Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg. `,
-      },
-      {
-        id: 3,
-        title: "Bibimbap",
-        category: "Korea",
-        price: 8.99,
-        img:
-          "https://dwellbymichelle.com/wp-content/uploads/2020/05/DWELL-bibimbap.jpg",
-        desc: `Boiling vegetables, serving with special hot sauce`,
-      },
-      {
-        id: 4,
-        title: "Dan Dan Mian",
-        category: "China",
-        price: 5.99,
-        img:
-          "https://www.savingdessert.com/wp-content/uploads/2019/02/Dan-Dan-Noodles-10.jpg",
-        desc: `Dan dan noodle, serving with green onion `,
-      },
-      {
-        id: 5,
-        title: "Yangzhou Fried Rice",
-        category: "China",
-        price: 12.99,
-        img:
-          "https://salu-salo.com/wp-content/uploads/2013/02/Yangzhou-Fried-Rice1.jpg",
-        desc: `Yangzhou style fried rice, serving with bean and pickles `,
-      },
-      {
-        id: 6,
-        title: "Onigiri",
-        category: "Japan",
-        price: 9.99,
-        img:
-          "https://www.manusmenu.com/wp-content/uploads/2017/08/Onigiri-3-1-of-1.jpg",
-        desc: `Rice Sandwich, serving with soy sauce`,
-      },
-      {
-        id: 7,
-        title: "Jajangmyeon",
-        category: "Korea",
-        price: 15.99,
-        img:
-          "https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-720x540.jpg",
-        desc: `Black bean sauce noodle, serving with green onion `,
-      },
-      {
-        id: 8,
-        title: "Ma Yi Shang Shu",
-        category: "China",
-        price: 12.99,
-        img:
-          "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/F688C2F6-86EC-46C4-B9C7-A6BA01DF7437/Derivates/32E3E72A-F786-406D-AF7F-B30980A9AC6C.jpg",
-        desc: `Hot pepper sauce noodle, serving with soy bean and onion`,
-      },
-      {
-        id: 9,
-        title: "Doroyaki",
-        category: "Japan",
-        price: 3.99,
-        img:
-          "https://www.justonecookbook.com/wp-content/uploads/2011/10/Dorayaki-New-500x400.jpg",
-        desc: `Red bean paste dessert, serving with honey.`,
-      },
-]
+  {
+    id: 1,
+    title: "Tteokbokki",
+    category: "Korea",
+    price: 10.99,
+    img: "https://twoplaidaprons.com/wp-content/uploads/2020/09/tteokbokki-top-down-view-of-tteokbokki-in-a-bowl-500x500.jpg",
+    desc: `Spicy rice cakes, serving with fish cake.`,
+  },
+  {
+    id: 2,
+    title: "Chicken Ramen",
+    category: "Japan",
+    price: 7.99,
+    img: "https://www.forkknifeswoon.com/wp-content/uploads/2014/10/simple-homemade-chicken-ramen-fork-knife-swoon-01.jpg",
+    desc: `Chicken noodle soup, serving with vegetables such as soy bean, green onion. In an optional you can ask for egg. `,
+  },
+  {
+    id: 3,
+    title: "Bibimbap",
+    category: "Korea",
+    price: 8.99,
+    img: "https://dwellbymichelle.com/wp-content/uploads/2020/05/DWELL-bibimbap.jpg",
+    desc: `Boiling vegetables, serving with special hot sauce`,
+  },
+  {
+    id: 4,
+    title: "Dan Dan Mian",
+    category: "China",
+    price: 5.99,
+    img: "https://www.savingdessert.com/wp-content/uploads/2019/02/Dan-Dan-Noodles-10.jpg",
+    desc: `Dan dan noodle, serving with green onion `,
+  },
+  {
+    id: 5,
+    title: "Yangzhou Fried Rice",
+    category: "China",
+    price: 12.99,
+    img: "https://salu-salo.com/wp-content/uploads/2013/02/Yangzhou-Fried-Rice1.jpg",
+    desc: `Yangzhou style fried rice, serving with bean and pickles `,
+  },
+  {
+    id: 6,
+    title: "Onigiri",
+    category: "Japan",
+    price: 9.99,
+    img: "https://www.manusmenu.com/wp-content/uploads/2017/08/Onigiri-3-1-of-1.jpg",
+    desc: `Rice Sandwich, serving with soy sauce`,
+  },
+  {
+    id: 7,
+    title: "Jajangmyeon",
+    category: "Korea",
+    price: 15.99,
+    img: "https://www.curiouscuisiniere.com/wp-content/uploads/2020/04/Jajangmyeon-Korean-Noodles-in-Black-Bean-Sauce5.1200H-720x540.jpg",
+    desc: `Black bean sauce noodle, serving with green onion `,
+  },
+  {
+    id: 8,
+    title: "Ma Yi Shang Shu",
+    category: "China",
+    price: 12.99,
+    img: "https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/ras/Assets/F688C2F6-86EC-46C4-B9C7-A6BA01DF7437/Derivates/32E3E72A-F786-406D-AF7F-B30980A9AC6C.jpg",
+    desc: `Hot pepper sauce noodle, serving with soy bean and onion`,
+  },
+  {
+    id: 9,
+    title: "Doroyaki",
+    category: "Japan",
+    price: 3.99,
+    img: "https://www.justonecookbook.com/wp-content/uploads/2011/10/Dorayaki-New-500x400.jpg",
+    desc: `Red bean paste dessert, serving with honey.`,
+  },
+];
 
-//Add categories in object to categories array
-menu.forEach(item => {
-    if (!categories.includes(item.category) && item.category != undefined) { 
-        categories.push(item.category)
-    }
-})
+menu.map((item) => {
+  !categories.includes(item.category) ? categories.push(item.category) : "";
+});
 
-//Create a button for each categories item
-categories.forEach(item => {
-    buttonContainer.innerHTML += `<button class="btn btn-dark rounded-pill menu-button" data-category=${item}>${item}</button>`
-})
+categories.map((item) => {
+  buttonContainer.innerHTML += `<button class="btn btn-dark rounded-pill menu-button" data-category=${item}>${item}</button>`;
+});
 
-const categoryButton = document.querySelectorAll("#buttons button")
+const categoryButton = document.querySelectorAll("#buttons button");
 
-//List the dishes that have same category with data-category got from button
-categoryButton.forEach(button => {
-    button.addEventListener('click', function(){
-        menuContainer.innerHTML = ''
-        menu.forEach(item => {
-            if (item.category == button.dataset.category) {
-                menuContainer.innerHTML += `
-                    <div class="menu col-12 col-lg-5 p-1 p-lg-2">
-                        <div class="menu__img">
-                            <img src="${item.img}" alt="${item.title}">
-                        </div>
-                        <div class="menu__info">
-                            <div class="menu__info-header">
-                                <h5 class="m-0">${item.title}</h5>
-                            </div>
-                            <div class="menu__info-body">
-                                <p class="m-0">${item.desc}</p>
-                            </div>
-                            <div class="menu__info-footer">
-                                <h5 class="m-0">${item.price}$</h5>
-                            </div>
-                        </div>
-                    </div>
-                `
-            } else if (button.dataset.category == "All") {
-                listAll()
-            }
-        })
-    })
-})
-
-function listAll() {
-    menuContainer.innerHTML = ''
-    menu.forEach(item => {
+categoryButton.forEach((button) => {
+  button.addEventListener("click", function () {
+    menuContainer.innerHTML = "";
+    menu.map((item) => {
+      if (
+        item.category == button.dataset.category ||
+        button.dataset.category == "All"
+      ) {
         menuContainer.innerHTML += `
         <div class="menu col-12 col-lg-5 p-1 p-lg-2">
-            <div class="menu__img">
-                <img src="${item.img}" alt="Cuisine ${item.id}">
+          <div class="menu__img">
+            <img src="${item.img}" alt="${item.title}">
+          </div>
+          <div class="menu__info">
+            <div class="menu__info-header">
+              <h5 class="m-0">${item.title}</h5>
             </div>
-            <div class="menu__info">
-                <div class="menu__info-header">
-                    <h5 class="m-0">${item.title}</h5>
-                </div>
-                <div class="menu__info-body">
-                    <p class="m-0">${item.desc}</p>
-                </div>
-                <div class="menu__info-footer">
-                    <h5 class="m-0">${item.price}$</h5>
-                </div>
+            <div class="menu__info-body">
+              <p class="m-0">${item.desc}</p>
             </div>
+            <div class="menu__info-footer">
+              <h5 class="m-0">${item.price}$</h5>
+            </div>
+          </div>
         </div>
-    `
-    })
-}
+        `;
+      }
+    });
+  });
 
-listAll()
+  if (button.dataset.category == "All") {
+    button.click();
+  }
+});
